0441f51789372c5a2b8d77daf477c34a02f12f2a
Irshad Cholakkal <irshadc.armino@gmail.com>
2025-01-13 17:46:12 +0530

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async{
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-);
+   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+// );
   runApp(const MyApp());
 }
 
 
-// ...
 
 
 class MyApp extends StatelessWidget {
